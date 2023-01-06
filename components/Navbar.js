b090a20674cd4f070ec6ab71334384ca089dd74c
Muhammad <me@kawijayaa.me>
2023-01-07 02:00:36 +0700

    main page barebone

---
+++ components/Navbar.js
@@ -0,0 +1,9 @@
+export default function Navbar() {
+    return (
+      <div>
+        <div className="fixed top-8 left-10 rounded-full bg-white py-2 px-8 w-[calc(100vw-100px)]">
+            <img src="/logo-plain.png" className="w-12"></img>
+        </div>
+      </div>
+    )
+  }
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import '../styles/globals.css'
+import Navbar from "../components/Navbar"
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="h-screen">
+      <Navbar />
+      <Component {...pageProps} />
+    </div>
+  )
 }
