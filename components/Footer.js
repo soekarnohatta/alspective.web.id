cb1899328252c4fed2430642966edec56c323c9e
Muhammad <me@kawijayaa.me>
2023-01-09 17:37:54 +0700

    about us done

--- components/Footer.js
+++ components/Footer.js
@@ -1,4 +1,5 @@
 import {FaTiktok, FaInstagram} from "react-icons/fa"
+import Link from "next/link"
 import {Inter} from "@next/font/google"
 
 const inter = Inter()
@@ -8,19 +9,21 @@ export default function Footer() {
         <div className={inter.className}>
             <div className="bg-[#000627] h-20 text-white px-8 grid grid-cols-3 justify-between items-center">
                 <div>
-                    ©2022 <br></br>
+                    © 2022 <br></br>
                     Kedubes Expo 2023
                 </div>
 
                 <div className="flex justify-center">
-                    <img src="/logo_simple.png" className="w-14 justify-center"></img>
+                    <Link href="/">
+                        <img src="/logo_simple.png" className="w-14 justify-center"></img>
+                    </Link>
                 </div>
 
                 <div className="grid justify-items-end gap-1">
                     <p>Find us on social media!</p>
                     <div className="flex gap-2 text-xl">
                         <FaTiktok />
-                        <FaInstagram />
+                        <a href="https://www.instagram.com/kepokedubes" target="_blank"><FaInstagram /></a>
                     </div>
                 </div>
             </div>
--- components/Navbar.js
+++ components/Navbar.js
@@ -3,8 +3,8 @@ import Link from "next/link";
 export default function Navbar() {
     return (
       <div>
-        <div className="shadow-lg shadow-white/25 fixed flex justify-between top-8 left-10 rounded-full bg-white py-2 px-8 w-[calc(100vw-100px)]">
-              <Link href="/"><img src="/logo_simple.png" className="w-12"></img></Link>
+        <div className="shadow-lg shadow-white/25 fixed flex justify-between top-8 left-10 rounded-full bg-white py-2 px-8 w-[calc(100vw-100px)] z-10">
+            <Link href="/"><img src="/logo_simple.png" className="w-12"></img></Link>
 
             <ul className="flex items-center gap-14 text-xl font-ethnocentric">
               <li>
