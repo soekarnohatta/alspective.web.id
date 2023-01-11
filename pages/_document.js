61d6f3528c4f8062c97a240b0e7cf54cc24279a6
Muhammad <me@kawijayaa.me>
2023-01-11 17:55:06 +0700

    add favicon and title

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico?v=2" />
+        <title>Kedubes Expo 2023</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
