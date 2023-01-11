6a1ba660999ba24fde6088022e4b5bbe9b616011
Muhammad <me@kawijayaa.me>
2023-01-11 17:56:28 +0700

    add meta description

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,7 @@ export default function Document() {
       <Head>
         <link rel="shortcut icon" href="/favicon.ico?v=2" />
         <title>Kedubes Expo 2023</title>
+        <meta name="description" content="Kedubes Expo 2023 atau biasa disingkat KEPO adalah acara tahunan yang menjadi sarana pemberian informasi kepada siswa/i SMAN 12 Jakarta terkait berbagai program studi di perguruan tinggi. Kedubes Expo 2023 bertujuan memudahkan dalam menentukan pilihan masa depan untuk ke jenjang berikutnya dan diselenggarakan oleh Alumni SMAN 12 Jakarta." />
       </Head>
       <body>
         <Main />
