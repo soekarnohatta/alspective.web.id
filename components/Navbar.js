6598c1bbf0608f3c8b687894655776a4b205e6b0
Muhammad <me@kawijayaa.me>
2023-01-11 23:01:17 +0700

    change navbar menu button on click

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,13 +1,15 @@
 import Link from "next/link";
 import {useState} from "react";
 import {GiHamburgerMenu} from "react-icons/gi";
+import {BsFillTriangleFill} from "react-icons/bs";
 
 export default function Navbar() {
   const [navbar, toggleNavbar] = useState(false);
+
     return (
       <div>
         <div className="shadow-lg shadow-white/25 fixed flex justify-between top-8 xs:left-[1.55rem] lg:left-[2.6rem] rounded-full bg-white py-2 xs:px-6 lg:px-8 xs:w-[calc(100vw-50px)] lg:w-[calc(100vw-100px)] z-20">
-            <Link href="/"><img src="/logo_simple.png" className="w-12"></img></Link>
+            <Link href="/" onClick={() => toggleNavbar(false)} ><img src="/logo_simple.png" className="w-12"></img></Link>
 
             <ul className="xs:hidden lg:flex items-center gap-14 text-xl font-ethnocentric">
               <li>
@@ -18,16 +20,17 @@ export default function Navbar() {
               </li>
             </ul>
 
-            <GiHamburgerMenu className="self-center text-4xl lg:hidden" onClick={() => toggleNavbar(!navbar)}/>
+            <GiHamburgerMenu className={`self-center text-4xl lg:hidden ${navbar ? 'hidden' : 'block'}`} onClick={() => toggleNavbar(!navbar)}/>
+            <BsFillTriangleFill className={`self-center text-2xl rotate-180 lg:hidden ${navbar ? 'block' : 'hidden'}`} onClick={() => toggleNavbar(!navbar)}/>
         </div>
-        <div className={`fixed top-20 left-[3rem] bg-gray-200/95 backdrop-blur-lg w-[calc(100vw-100px)] shadow-lg shadow-white/25 rounded-xl xs:${navbar ? 'flex' : 'hidden'} lg:hidden z-10`}>
+        <div className={`fixed top-20 left-[2.5rem] bg-gray-200/95 backdrop-blur-lg w-[calc(100vw-80px)] shadow-lg shadow-white/25 rounded-xl xs:${navbar ? 'flex' : 'hidden'} lg:hidden z-10`}>
           <ul className="flex flex-col gap-4 text-xl font-ethnocentric pt-10 pb-6 px-6">
             <li>
-              <Link href="/infoptn" onClick={() => toggleNavbar(!navbar)}>Info PTN</Link>
+              <Link href="/infoptn" onClick={() => toggleNavbar(false)}>Info PTN</Link>
             </li>
-            <hr class="h-px w-[calc(100vw-155px)] bg-gray-400 border-0"></hr>
+            <hr className="h-px w-[calc(100vw-135px)] bg-gray-400 border-0"></hr>
             <li>
-              <Link href="/about" onClick={() => toggleNavbar(!navbar)}>About Us</Link>
+              <Link href="/about" onClick={() => toggleNavbar(false)}>About Us</Link>
             </li>
           </ul>
         </div>
